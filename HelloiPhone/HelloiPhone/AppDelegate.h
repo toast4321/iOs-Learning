fccc0b0eabe7adf345245d83a6ae2dce9cfcc993
Tosin Onafowokan <toast4321@gmail.com>
2012-01-13 01:30:42 -0500

    first commit

---
+++ HelloiPhone/HelloiPhone/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HelloiPhone
+//
+//  Created by Tosin Onafowokan on 1/12/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HelloiPhone/HelloiPhone/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  HelloiPhone
+//
+//  Created by Tosin Onafowokan on 1/12/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
